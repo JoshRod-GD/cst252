d5dc7c3972c3126d22b8eb86df50bf54734893d1
JoshRod-GD <80925863+JoshRod-GD@users.noreply.github.com>
2021-10-18 12:21:25 -0700

    added lab 14

---
+++ lab14/js/lab.js
@@ -0,0 +1,9 @@
+//@authors Joshua Rodriguez, Matthew Habrial
+//@Date 10/18/21
+
+
+function FizzBuzz(maxNums, factorObj){
+    for (var num= 0; num<maxNums; num++){
+      var = outputStr = "";
+    }
+}
